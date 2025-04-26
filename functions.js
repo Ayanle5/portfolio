ffcc808e07c18e7cba9dbaaa9bd4116a83fa365d
Ayanle <ayanle3645@gmail.com>
2025-04-26 21:14:33 +0100

    a

--- functions.js
+++ functions.js
@@ -3,3 +3,20 @@ window.addEventListener('scroll', function() {
     document.getElementById('background').style.transform = `translateY(${offset * 0.2}px)`;
   });
   
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
